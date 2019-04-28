2b69d2e5554e59d23617c76ac9274dac311574e7
shaoguoji <752147916@qq.com>
2019-04-28 10:37:17 +0800

    修改 ipmode 写入 if 判断逻辑

--- src/ip/at_ipmode.c
+++ src/ip/at_ipmode.c
@@ -11,14 +11,11 @@ static at_result_t at_ipmode_query(void)
 extern int msh_exec(char *cmd, rt_size_t length);
 static at_result_t at_ipmode_setup(const char *args)
 {
-    if(at_req_parse_args(args, "=%d", &ipmode) > 0)
-    {
-        
-    }
-    else
+    if(at_req_parse_args(args, "=%d", &ipmode) <= 0)
     {
         return AT_RESULT_PARSE_FAILE;
     }
+
     return AT_RESULT_OK;
 }
 
